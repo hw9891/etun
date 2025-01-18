c92ddd2b6e61a19fc0174183cf3cfee7e066bfa1
hw9891 <hw9891.gmail.com>
2025-01-18 20:13:45 +0800

    init

--- index.js
+++ index.js
@@ -112,7 +112,7 @@ export default {
 			const upgradeHeader = request.headers.get('Upgrade');
 			const hostName = request.headers.get('Host');
 			const reqPath = new URL(request.url).pathname.toLowerCase();
-			const re = new RegExp("^/sub?target");
+			const re = new RegExp("^/sub");
 			if (!upgradeHeader || upgradeHeader !== 'websocket') {
 				if (reqPath == `/`) {
 					return new Response(JSON.stringify(request.cf, null, 4), {
